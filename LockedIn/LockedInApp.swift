088090803b612e6714478dcfdc3991f32e457e22
Marianne <m.ninet@protonmail.com>
2025-12-03 20:50:11 +0100

    Initial Commit

---
+++ LockedIn/LockedInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockedInApp.swift
+//  LockedIn
+//
+//  Created by Marianne Ninet on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LockedInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LockedInTests/LockedInTests.swift
@@ -0,0 +1,16 @@
+//
+//  LockedInTests.swift
+//  LockedInTests
+//
+//  Created by Marianne Ninet on 03/12/2025.
+//
+
+import Testing
+
+struct LockedInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
